e25029e11fc7bca990729620c3322b325e13c354
neil patel <neil@myindianweddingapp.com>
2015-04-04 17:22:55 +0100

    added purchase show pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   # Support Stripe Payments through charges
 
   resources :charges
+
+  # Route for showing purchase
+  resources :purchases, only: [:show]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
